700b261da1861e5133d3ecef2c6d5cd9b32a074d
SoumyaRanjan63 <130931666+SoumyaRanjan63@users.noreply.github.com>
2023-07-03 23:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+     const form = document.getElementById("form1");
+	 const firstName=form.firstName.Value;
+     const lastName=form.lastName.value;
+	alert('First Name: ${firstName} \n Last Name: ${lastName}');
 }
